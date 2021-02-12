f98448bbac2033a3bc7cee1d15a7ac01ffa3f5e4
maxly20 <maxly2011@gmail.com>
2021-02-12 13:18:25 +1100

    adding responsive to landing page

--- app.js
+++ app.js
@@ -30,3 +30,25 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = 'flex';
   dots[slideIndex - 1].className += ' active';
 }
+
+// BURGER
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    burger.classList.toggle('toggle');
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = '';
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+  });
+};
+
+navSlide();
